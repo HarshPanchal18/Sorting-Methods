bb06448d52b8fa3d8fe0e03a3db391ad5bdb3cd0
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2021-10-11 15:12:03 +0530

    Add files via upload

---
+++ SLCT_SOR.C
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <conio.h>
+
+void swap(int *a, int *b);
+
+void selectionSort(int arr[], int size)
+{
+	int i, j;
+	for (i = 0; i < size; i++)
+	{
+		for (j = i; j < size; j++)
+		{
+			if (arr[i] > arr[j])
+				swap(&arr[i], &arr[j]);
+		}
+	}
+}
+
+void swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main()
+{
+	int arr[10], i, size;
+	//clrscr();
+	printf("\nHow many numbers you want to sort??: ");
+	scanf("%d", &size);
+	printf("\nEnter %d numbers: \n", size);
+	for (i = 0; i < size; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	selectionSort(arr, size);
+	printf("\nSorted array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	getch();
+	return 0;
+}
