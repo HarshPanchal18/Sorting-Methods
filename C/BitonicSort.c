27329632fbc3ad4dfe0b6169840f5385e05f92a6
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-02 17:21:03 +0530

    Made changes and commented

--- C/BitonicSort.c
+++ C/BitonicSort.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-#define MAX 5
+// This algorithm only works on even numbers of elements.
 
 void compare(int data[], int i, int j, int dir)
 {
-    int t;
-    if (dir == (data[i] >= data[j]))
+    /*The parameter dir indicates the sorting direction, ASCENDING
+    or DESCENDING; if (a[i] > a[j]) agrees with the direction,
+    then a[i] and a[j] are interchanged.*/
+    if (dir == (data[i] > data[j]))
     {
-        t = data[i];
+        int t = data[i];
         data[i] = data[j];
         data[j] = t;
     }
@@ -15,12 +17,16 @@ void compare(int data[], int i, int j, int dir)
 
 void bitonicMerge(int data[], int low, int c, int dir)
 {
-    int k, i;
+    /*It recursively sorts a bitonic sequence in ascending order,
+    if dir = 1, and in descending order otherwise (means dir=0).
+    The sequence to be sorted starts at index position low,
+    the parameter cnt is the number of elements to be sorted.*/
     if (c > 1)
     {
-        k = c / 2;
-        for (i = low; i <= low + k; i++)
+        int k = c / 2;
+        for (int i = low; i <= low + k; i++)
             compare(data, i, i + k, dir);
+        
         bitonicMerge(data, low, k, dir);
         bitonicMerge(data, low + k, k, dir);
     }
@@ -28,10 +34,12 @@ void bitonicMerge(int data[], int low, int c, int dir)
 
 void bitonicSort(int data[], int low, int c, int dir)
 {
-    int k;
+    /* This function first produces a bitonic sequence by recursively
+    sorting its two halves in opposite sorting orders, and then
+    calls bitonicMerge to make them in the same order */
     if (c > 1)
     {
-        k = c / 2;
+        int k = c / 2;
         bitonicSort(data, low, k, 1);
         bitonicSort(data, low + k, k, 0);
         bitonicMerge(data, low, c, dir);
@@ -40,16 +48,19 @@ void bitonicSort(int data[], int low, int c, int dir)
 
 int main(void)
 {
-    int i;
-    int data[MAX];
+    int n;
 
-    printf("\nEnter %d Elements:\n", MAX);
-    for (i = 0; i < MAX; i++)
+    printf("\nEnter the number of elements: ");
+    scanf("%d",&n);
+    
+    int data[n];
+    printf("\nEnter %d Elements:\n", n);
+    for (i = 0; i < n; i++)
         scanf("%d", &data[i]);
 
-    bitonicSort(data, 0, MAX, 1); // 1 for Ascending, 0 for Descending
+    bitonicSort(data, 0, n, 1); // 1 for Ascending, 0 for Descending
 
     printf("\nSorted\n");
-    for (i = 0; i < MAX; i++)
+    for (i = 0; i < n; i++)
         printf("%d\t", data[i]);
 }
