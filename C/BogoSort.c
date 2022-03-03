73f15d7230bff7b73c38315abb2b44becf0e12b1
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-03 11:50:44 +0530

    Updated code and Comments

--- C/BogoSort.c
+++ C/BogoSort.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 5
+/*
+ *      Bogosort (also known as permutation sort, stupid sort, slowsort,
+ *      shotgun sort, random sort, monkey sort, bobosort or shuffle sort) is a highly inefficient
+ *      sorting algorithm based on the generate and test paradigm. Two versions of this algorithm
+ *      exist: a deterministic version that enumerates all permutations until it hits a sorted one,
+ *      and a randomized version that randomly permutes its input.
+ *      Randomized version is implemented here.
+ *
+ *      ### Algorithm
+ *      Shuffle the array untill array is sorted.
+ */
+
 int isSorted(int *a, int n)
 {
     while (--n >= 1)
-    {
         if (a[n] < a[n - 1])
             return 0;
-    }
     return 1;
 }
 
 void shuffle(int *a, int n)
 {
-    int i, t, temp;
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        t = a[i];
-        temp = rand() % n;
+        int t = a[i];
+        int temp = rand() % n;
         a[i] = a[temp];
         a[temp] = t;
     }
@@ -32,13 +40,18 @@ void BogoSort(int *a, int n)
 
 int main(void)
 {
-    int no[MAX];
-    int i;
-    printf("\nEnter the elements:\n");
-    for (i = 0; i < MAX; i++)
+    int n;
+    printf("\nEnter the number of elements: ");
+    scanf("%d",&n);
+    
+    int no[n];
+    printf("\nEnter %d elements:\n",n);
+    for (int i = 0; i < n; i++)
         scanf("%d", &no[i]);
-    BogoSort(no, MAX);
+    
+    BogoSort(no, n);
+    
     printf("\nSorted list:\n");
-    for (i = 0; i < MAX; i++)
+    for (int i = 0; i < n; i++)
         printf("%d ", no[i]);
 }
