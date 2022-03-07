b5f22c95b55de8f90d2562e12f7bb8bf7521dcb4
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-07 11:12:03 +0530

    Reformatted code

--- C/OddEvenSort.C
+++ C/OddEvenSort.C
@@ -1,38 +1,19 @@
 #include <stdio.h>
-#include <conio.h>
-#define MAX 5
-void oddeven_sort(int *);
-void swap(int *x, int *y);
-int main(void)
-{
-	int a[MAX], i;
-	printf("Enter %d elements: \n", MAX);
-	for (i = 0; i < MAX; i++)
-		scanf("%d", &a[i]);
-
-	printf("Sorted elements are:\n");
-	oddeven_sort(a);
-
-	for (i = 0; i < MAX; i++)
-		printf("%d\t", a[i]);
-	getch();
-}
 
 void swap(int *x, int *y)
 {
-	int temp;
-	temp = *x;
+	int temp = *x;
 	*x = *y;
 	*y = temp;
 }
 
-void oddeven_sort(int *x)
+void oddeven_sort(int *x,int n)
 {
 	int sort = 0, i;
 	while (!sort)
 	{
 		sort = 1;
-		for (i = 1; i < MAX; i += 2)
+		for (i = 1; i < n - 1; i += 2)
 		{
 			if (x[i] > x[i + 1])
 			{
@@ -40,7 +21,7 @@ void oddeven_sort(int *x)
 				sort = 0;
 			}
 		}
-		for (i = 0; i < MAX - 1; i += 2)
+		for (i = 0; i < n - 1; i += 2)
 		{
 			if (x[i] > x[i + 1])
 			{
@@ -49,4 +30,21 @@ void oddeven_sort(int *x)
 			}
 		}
 	}
+}
+
+int main(void)
+{
+	printf("\nEnter the number of elements: ");
+	scanf("%d", &n);
+	
+	int a[n];
+	printf("Enter %d elements: \n", MAX);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	printf("Sorted elements are:\n");
+	oddeven_sort(a, n);
+
+	for (int i = 0; i < n; i++)
+		printf("%d\t", a[i]);
 }
