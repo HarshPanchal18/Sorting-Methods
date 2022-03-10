c3a47d021cbdb4d6f177346e9a29d0d5071d07e8
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-10 11:11:37 +0530

    Update ShellSort.C

--- C/ShellSort.C
+++ C/ShellSort.C
@@ -1,45 +1,45 @@
 //Shell Sort 
 #include <stdio.h>
-#include <conio.h>
-#define MAX 20
-int main()
+
+int main(void)
 {
-	int arr[MAX], i, j, k, n, incr;
-	//clrscr();
+	int i, k, n, incr;
 	printf("\nEnter the number of elements: ");
 	scanf("%d", &n);
+	int arr[n];
+	
 	for (i = 0; i < n; i++)
 	{
 		printf("\nEnter element %d: ", i + 1);
 		scanf("%d", &arr[i]);
 	}
-	//clrscr();
+	
 	printf("\nUnsorted list is: ");
 	for (i = 0; i < n; i++)
 		printf("%d ", arr[i]);
+	
 	printf("\nEnter maximum increment (odd)");
 	scanf("%d", &incr);
 
 	while (incr >= 1)
 	{
-		for (j = incr; j < n; j++)
+		for (int j = incr; j < n; j++)
 		{
 			k = arr[j];
 			for (i = j - incr; i >= 0 && k < arr[i]; i = i - incr)
-			{
-				arr[i + incr] = arr[i];
-			}
+				arr[i + incr] = arr[i];			
 			arr[i + incr] = k;
 		}
+		
 		printf("\nIncrement = %d\n", incr);
 		for (i = 0; i < n; i++)
 			printf("%d ", arr[i]);
 		printf("\n");
+		
 		incr = incr - 2;
 	}
+	
 	printf("\nSorted list is : ");
 	for (i = 0; i < n; i++)
 		printf("%d ", arr[i]);
-	getch();
-	return 0;
 }
