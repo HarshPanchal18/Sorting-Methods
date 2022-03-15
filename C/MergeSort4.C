fd319082c68dff44cd93ef4c2cbb5dbc259036a1
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-15 18:28:17 +0530

    Update MergeSort4.C

--- C/MergeSort4.C
+++ C/MergeSort4.C
@@ -45,3 +45,64 @@ void mergeSort(int a[], int b[], int n, int m)
     }
 }
 
+
+int main(void)
+{
+    int n, m;
+    printf("\nEnter the number of elements first array: ");
+    scanf("%d", &n);
+
+    printf("\nEnter the number of elements second array: ");
+    scanf("%d", &m);
+
+    int capacity = 100;
+    int used = 0;
+
+    int *a = (int *)malloc(sizeof(int) * capacity);
+    int *b = (int *)malloc(sizeof(int) * capacity);
+
+    printf("\nEnter the elements of first array:\n");
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &a[i - 1]);
+        if (used == capacity)
+        {
+            capacity *= 2;
+            a = (int *)realloc(a, capacity * sizeof(int));
+        }
+        a[used] = a[i - 1];
+        used++;
+    }
+
+    printf("\nEnter the elements of second array:\n");
+    for (int i = 1; i <= m; i++)
+    {
+        scanf("%d", &b[i - 1]);
+        if (used == capacity)
+        {
+            capacity *= 2;
+            b = (int *)realloc(b, capacity * sizeof(int));
+        }
+        b[used] = b[i - 1];
+        used++;
+    }
+
+    mergeSort(a, b, n, m);
+    print(a, b, n, m);
+}
+
+// algorithm for merging two sorted array in O(1) space complexity and in
+// Nlog(N) + Mlog(M) time complexity
+/*
+Sample input/output
+Enter size of first array:
+6
+Enter size of second array:
+5
+Enter array elements:
+1 5 7 9 13 17
+Enter array elements:
+4 6 10 13 15
+output
+1 4 5 6 7 9 10 13 13 15 17
+*/
