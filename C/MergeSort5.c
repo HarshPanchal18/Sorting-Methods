fc50026009d38a03bc146a7c6e8d9af0969413ed
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-23 11:36:09 +0530

    Update MergeSort5.c

--- C/MergeSort5.c
+++ C/MergeSort5.c
@@ -78,3 +78,67 @@ void merge(int arr[], int beg, int midA, int midB, int end, int temp[])
     while (k < end)
         temp[l++] = arr[k++];
 }
+
+void three_way_merge_sort(int arr[], int beg, int end, int temp[])
+{
+    // If the array contains only a single element, then return
+    if (end - beg < 2)
+        return;
+
+    // Split the array into three parts
+    int midA = beg + ((end - beg) / 3);
+    int midB = beg + 2 * ((end - beg) / 3) + 1;
+
+    three_way_merge_sort(temp, beg, midA, arr);
+    three_way_merge_sort(temp, midA, midB, arr);
+    three_way_merge_sort(temp, midB, end, arr);
+
+    // Merge the sorted arrays
+    merge(temp, beg, midA, midB, end, arr);
+}
+
+int main()
+{
+    int n;
+    printf("\nEnter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    if (n <= 0)
+    {
+        printf("There are no numbers to sort!!!");
+        return 0;
+    }
+
+    printf("Enter %d numbers:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int temp[n];
+
+    // Copy the elements of the temporary elements into the new array
+    for (int i = 0; i < n; i++)
+        temp[i] = arr[i];
+
+    three_way_merge_sort(temp, 0, n, arr);
+
+    printf("The numbers in sorted order is: ");
+    for (int i = 0; i < n; i++)
+        printf(" %d ", temp[i]);
+    printf("\n");
+
+    return 0;
+}
+
+/*
+SAMPLE INPUT AND OUTPUT
+
+SAMPLE 1
+Enter the number of elements: 5
+Enter the numbers: 1 3 5 2 4
+The numbers in sorted order is:  1  2  3  4  5
+
+SAMPLE 2
+Enter the number of elements: 0
+There are no numbers to sort!!!
+*/
